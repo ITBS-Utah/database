7a1137fbc04510a024889a5b4698290a3c05ecd4
Russell2259 <84256826+Russell2259@users.noreply.github.com>
2022-12-28 22:12:51 -0700

    main commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+const fs = require('fs/promises')
 const express = require('express');
 const cors = require('cors');
 const { v4: uuid } = require('uuid');
-const { WebSocketServer } = require("ws");
 
 const whitelist = ['https://support.itbsutah.com']
 
@@ -17,20 +17,50 @@ var corsOptionsDelegate = function (req, callback) {
 
 const app = express();
 
+const expressWs = require('express-ws')(app);
+
 app.use(express.json());
 app.use(cors(corsOptionsDelegate));
 
+app.ws('/', (ws, req) => {
+    let methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
 
-const server = app.listen(3000, () => {
-    console.log('ITBS Utah Database server is running');
+    ws.on('message', async (rawData) => {
+        const data = JSON.parse(rawData)
+
+        if (data.method && data.path && data.reqToken) {
+            if (methods.includes(data.method)) {
+                if (data.method === 'GET') {
+                    let file;
+
+                    try {
+                        file = await fs.readFile(`./db${data.path}`);
+                        const parsedFile = JSON.parse(file);
+
+                        ws.send(JSON.stringify({
+                            error: false,
+                            reqToken: data.reqToken,
+                            data: parsedFile
+                        }));
+                    } catch (err) {
+                        console.log(err)
+
+                        ws.send(JSON.stringify({
+                            error: true,
+                            errorMsg: err
+                        }))
+                    }
+                }
+            } else {
+                ws.send(JSON.stringify({
+                    error: true,
+                    errorMsg: 'Invalid method type'
+                }))
+            }
+        }
+    });
 });
 
-const wss = new WebSocketServer({ port: 9000 });
-
-wss.on('connection', function connection(ws) {
-    ws.on('message', function message(data) {
-        console.log(JSON.parse(data));
-    });
-
-    ws.send('something');
+const server = app.listen(9000, () => {
+    console.log('ITBS Utah Database server is running');
 });
